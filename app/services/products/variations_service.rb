70dd3390ab297a8625ee40dce2f3fc086e5c09bc
Hussain Niazi <hussainkhanniazi@hotmail.com>
2024-05-16 20:15:30 +0200

    I23: More fixes and tests

--- app/services/products/variations_service.rb
+++ app/services/products/variations_service.rb
@@ -14,12 +14,11 @@ module Products
     end
 
     def call
-      product = Product.find_by!(asin: @asin)
+      product = find_product
       return [] unless product.parent_asin.present?
-      return product.variation_asins if product.variation_asins.present? && (product.product_updated_at.nil? || product.product_updated_at < 1.week.ago)
+      return product.variation_asins if should_fetch_from_database?(product)
 
-      child_asins = find_child_asins(product)
-
+      child_asins = Array.wrap(find_child_asins(product))
       GetChildProductsJob.perform_later(child_asins)
 
       child_asins
@@ -29,14 +28,24 @@ module Products
 
     private
 
+    def find_product
+      Product.find_by!(asin: @asin)
+    end
+
+    def should_fetch_from_database?(product)
+      product.variation_asins.present? &&
+        product.product_updated_at.present? &&
+        product.product_updated_at > 1.week.ago
+    end
+
     def find_child_asins(product)
       parent_product = Product.find_by(asin: product.parent_asin)
-      if parent_product
-        parent_product.child_asins
-      else
-        parent_product = Products::SearchService.call(product.parent_asin).first
-        parent_product[:child_asins]
-      end
+      return parent_product.child_asins if parent_product &&
+                                           product.product_updated_at.present? &&
+                                           product.product_updated_at > 1.week.ago
+
+      parent_product = Products::SearchService.call(product.parent_asin).first
+      parent_product[:child_asins]
     end
   end
 end
--- test/integration/v1/products/variations_test.rb
+++ test/integration/v1/products/variations_test.rb
@@ -8,49 +8,86 @@ module Products
 
     setup do
       stub_rails_credentials
+      @asin = 'B0CMT86BZ7'
+      @parent_asin = 'B0CMYK2MFZ'
     end
 
     def variations_url(asin)
       "/v1/products/#{asin}/variations"
     end
 
-    test 'returns 202 when variations have not been saved to database' do
-      product = create(:product, asin: 'B0CMT86BZ7', parent_asin: 'B0CMYK2MFZ')
+    def perform_request(asin)
+      Products::AmzSpApiService.stub_const(:SUCCESSIVE_REQUEST_DELAY, 0) do
+        perform_enqueued_jobs do
+          get variations_url(asin)
+        end
+      end
+    end
+
+    test 'API returns and saves variations to database' do
+      product = create(:product, asin: @asin, parent_asin: @parent_asin)
 
       stub_parent_product_request
       stub_child_products_request1
       stub_child_products_request2
 
-      Products::AmzSpApiService.stub_const(:SUCCESSIVE_REQUEST_DELAY, 0) do
-        perform_enqueued_jobs do
-          get variations_url('B0CMT86BZ7')
-        end
-      end
+      perform_request(@asin)
 
       assert_response :success
 
       product.reload
       assert_equal 30, product.variation_asins.size
-      assert_equal 30, Product.where(parent_asin: 'B0CMYK2MFZ').size
+      assert_equal 30, Product.where(parent_asin: product.parent_asin).size
       assert_equal 30, response.parsed_body.count
     end
 
-    test 'returns variations when they exist in database' do
+    test 'database returns variations when they exist' do
       Products::SearchService.expects(:call).never
 
-      parent_product = create(:product, asin: 'B0CMYK2MFZ', child_asins: %w[B0CMT86BZ7 B0CMT86BZ8 B0CMT86BZ9])
-      first_child = create(:product, asin: 'B0CMT86BZ7', parent_asin: 'B0CMYK2MFZ',
-                                     variation_asins: %w[B0CMT86BZ7 B0CMT86BZ8 B0CMT86BZ9])
-      create(:product, asin: 'B0CMT86BZ8', parent_asin: 'B0CMYK2MFZ',
-                       variation_asins: %w[B0CMT86BZ7 B0CMT86BZ7 B0CMT86BZ9])
-      create(:product, asin: 'B0CMT86BZ9', parent_asin: 'B0CMYK2MFZ',
-                       variation_asins: %w[B0CMT86BZ7 B0CMT86BZ7 B0CMT86BZ9])
+      parent_product = create(:product,
+                              asin: @parent_asin,
+                              child_asins: %w[B0CMT86BZ7 B0CMT86BZ8 B0CMT86BZ9],
+                              product_updated_at: 1.day.ago)
+      first_child = create(:product,
+                           asin: @asin,
+                           parent_asin: @parent_asin,
+                           variation_asins: %w[B0CMT86BZ7 B0CMT86BZ8 B0CMT86BZ9],
+                           product_updated_at: 1.day.ago)
+      create(:product, asin: 'B0CMT86BZ8', parent_asin: @parent_asin,
+                       variation_asins: %w[B0CMT86BZ7 B0CMT86BZ7 B0CMT86BZ9], product_updated_at: 1.day.ago)
+      create(:product, asin: 'B0CMT86BZ9', parent_asin: @parent_asin,
+                       variation_asins: %w[B0CMT86BZ7 B0CMT86BZ7 B0CMT86BZ9], product_updated_at: 1.day.ago)
 
       get variations_url(first_child.asin)
 
       assert_response :success
 
       assert_equal parent_product.child_asins, response.parsed_body
+    end
+
+    test 'API returns child asins when product has not been updated for over a week' do
+      stub_parent_product_request
+      stub_child_products_request1
+      stub_child_products_request2
+
+      parent_product = create(:product, asin: @parent_asin, child_asins: %w[B0CMT86BZ7 B0CMT86BZ8 B0CMT86BZ9])
+      first_child = create(:product, asin: @asin, parent_asin: @parent_asin,
+                                     variation_asins: %w[B0CMT86BZ7 B0CMT86BZ8 B0CMT86BZ9],
+                                     product_updated_at: 2.weeks.ago)
+      create(:product, asin: 'B0CMT611Z5', parent_asin: @parent_asin)
+      create(:product, asin: 'B0CMT61M5X', parent_asin: @parent_asin)
+
+      assert_equal 3, first_child.variation_asins.size
+      assert_equal 3, Product.where(parent_asin: parent_product.asin).size
+
+      perform_request(first_child.asin)
+
+      assert_response :success
+
+      first_child.reload
+      assert_equal 30, first_child.variation_asins.size
+      assert_equal 30, Product.where(parent_asin: parent_product.asin).size
+      assert_equal 30, response.parsed_body.count
     end
   end
 end
